11e278890ef620727b5c5424cfa90e6ef83a5e40
Rao Komar <Raokomar@gmail.com>
2019-07-04 19:29:59 -0400

    adding contact forms

--- contacts/models.py
+++ contacts/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    messsage = models.TextField(blank=True)
+    message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
 
